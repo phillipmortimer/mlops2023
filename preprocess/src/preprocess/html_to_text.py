9fd19682ec41c35b98a44a414d141bc0ee4f6b08
phillipmortimer <phillip@phillipmortimer.com>
2023-05-21 23:44:31 +0100

    html_to_text and test

--- preprocess/src/preprocess/html_to_text.py
+++ preprocess/src/preprocess/html_to_text.py
@@ -1,2 +1,6 @@
-def html_to_text(html) -> str:
-    return ""
+from bs4 import BeautifulSoup
+
+
+def html_to_text(html: str) -> str:
+    parser = BeautifulSoup(html, features="html.parser")
+    return parser.get_text()
--- preprocess/tests/test_html_to_text.py
+++ preprocess/tests/test_html_to_text.py
@@ -1,5 +1,19 @@
+from pathlib import Path
+
+import pytest
+
 from preprocess.html_to_text import html_to_text
 
 
-def test_html_to_text():
-    assert html_to_text("a") == "a"
+@pytest.fixture
+def test_file():
+    test_path = Path(__file__).parent / "test_data/18563939_11.html"
+    with test_path.open() as fp:
+        text = fp.read()
+    return text
+
+
+def test_html_to_text(test_file):
+    target = "\n\n\nCash flows from operating activities\n\nNet income\n$839,534\n"
+
+    assert html_to_text(test_file)[:len(target)] == target
